d82b660b0189b1f4aefe3ff923f9267fd5b73d6e
dhef415 <dhef415@gmail.com>
2021-09-03 11:52:36 -0700

    cant get delete to return 404

--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -1,1 +1,75 @@
 // Write your "projects" router here!
+// All comments are notes for myself
+const express = require('express')
+const Projects = require('./projects-model')
+const router = express.Router()
+
+router.get('/', (req, res) => {
+    Projects.get()
+        .then(project => {
+            res.status(404).json(project)
+        })
+        .catch(res.status(404))
+})
+
+router.get('/:id', (req, res) => {
+    const {id} = req.params
+    Projects.get(id)
+        .then(byId => {
+            if (!byId) {
+                res.status(404).json({message: 'not found'}) // Needed a message to pass test
+            } else {
+                res.status(200).json(byId)
+            }
+        })
+        .catch(res.status(500))
+})
+
+router.post('/', (req, res) => {
+    Projects.insert(req.body)
+        .then(newProj => {
+            res.status(201).json(newProj)
+        })
+        .catch(() => {
+            res.status(400).json({message: 'missing data'})
+        })
+})
+
+router.put('/:id', (req, res) => {
+    const {id,} = req.params
+    Projects.update(id, req.body)
+        .then(changes => {
+            if (!id) {
+                res.status(404).json({message: 'does not exist'})
+            } else {
+                res.status(200).json(changes)
+            }
+        })
+        .catch(() => {
+            res.status(400).json({message: 'nope'})
+        })
+})
+
+// I always struggle with delete 
+router.delete('/:id', (req, res) => {
+    const {id} = req.params
+    Projects.get(id)
+        .then(toDelete => {
+            if (!id) {
+                res.status(404).json({message: 'no project with given id'})
+            } else {
+                Projects.remove(id)
+                    .then(
+                        res.status(200).json(toDelete)
+                    )
+                    .catch(err => {
+                        console.log(err)
+                    })
+            }
+        })
+        .catch(() => {
+            res.status(404).json({message: 'no'})
+        })
+})
+
+module.exports = router;
